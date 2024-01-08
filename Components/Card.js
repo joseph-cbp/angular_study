5236b0bacce2cf936a6d5ffe0c18f9ad376d7698
Joseph Pereira <joseph.carlosx@gmail.com>
2024-01-08 12:41:50 -0400

    fix: settings on 'shadow' var

--- Components/Card.js
+++ Components/Card.js
@@ -1,7 +1,22 @@
 class Card extends HTMLElement {
     constructor(){
         super();
+
+        const shadow = this.attachShadow({mode: "open"});
+        shadow.appendChild(this.build())
+        shadow.appendChild(this.style())
     }
-    build(){}
+
+    build(){
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card")
+
+        const left = document.createElement('div')
+        componentRoot.appendChild(left)
+
+        return componentRoot;
+    }
     style(){}
 }
+
+customElements.define('card-test', Card);
